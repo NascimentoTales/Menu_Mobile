bb948428c1812993403666457f1ff8fcbaa70685
NascimentoTales <nascimentosantostales@gmail.com>
2022-03-12 16:20:06 -0300

    Quase Pronto

---
+++ sripts/menu/menu.js
@@ -0,0 +1,5 @@
+const menuToggle = document.querySelector(".menu-toggle").addEventListener("click", () =>{
+    let header =document.getElementById("header")
+    header.classList.toggle("on")
+    
+})
